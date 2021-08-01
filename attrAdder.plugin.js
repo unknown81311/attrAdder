c9a6110d717f856b4628e0ec53f9649833382b56
unknown81311 <43104779+unknown81311@users.noreply.github.com>
2021-08-01 17:20:25 -0600

    Update attrAdder.plugin.js
    fixed performance and mac/linux

--- attrAdder.plugin.js
+++ attrAdder.plugin.js
@@ -2,17 +2,17 @@
  * @name attrAdder
  * @author unknown81311#6969
  * @description Adds attributes to elements, can be used for more features in themes.
- * @version 1.1
+ * @version 1.2
  * @invite https://discord.gg/yYJA3qQE5F
  * @authorId 359174224809689089
- * @source https://github.com/unknown81311/attrAdder
- * @updateUrl https://raw.githubusercontent.com/unknown81311/attrAdder/main/attrAdder.plugin.js
  */
+this.threadDATA = BdApi.findModuleByProps('getThreadSidebarState')
 
 module.exports = class attrAdder {
     load() {
         if(!document.body.hasAttribute('plugins')){
-            const bdpp = BdApi.Plugins.folder.split('\\').slice(0,-1).join('\\')+`\\data\\${process.env.DISCORD_RELEASE_CHANNEL}`;
+            const path = require("path");
+            const bdpp = path.resolve(path.dirname(BdApi.Plugins.folder), "data", DiscordNative.app.getReleaseChannel(), "plugins.json");
             this.watcher = require('fs').watch(bdpp, (eventType, filename) => {
                 document.body.setAttribute('plugins',BdApi.Plugins.getAll().map(p => p.id).filter(BdApi.Plugins.isEnabled));
             });
@@ -20,7 +20,8 @@ module.exports = class attrAdder {
     }
     start() {
         if(!document.body.hasAttribute('plugins')){
-            const bdpp = BdApi.Plugins.folder.split('\\').slice(0,-1).join('\\')+`\\data\\${process.env.DISCORD_RELEASE_CHANNEL}`;
+            const path = require("path");
+            const bdpp = path.resolve(path.dirname(BdApi.Plugins.folder), "data", DiscordNative.app.getReleaseChannel(), "plugins.json");
             this.watcher = require('fs').watch(bdpp, (eventType, filename) => {
                 document.body.setAttribute('plugins',BdApi.Plugins.getAll().map(p => p.id).filter(BdApi.Plugins.isEnabled));
             });
@@ -32,7 +33,7 @@ module.exports = class attrAdder {
     }
     observer (mutations) {
         if(document.querySelector('.threadSidebar-1o3BTy') && !document.querySelector('.threadSidebar-1o3BTy[id]')){
-            document.querySelector('.threadSidebar-1o3BTy').setAttribute('id',BdApi.findModuleByProps('getThreadSidebarState').getThreadSidebarState(BdApi.findModuleByProps('getChannelId').getChannelId())?.channelId);
+            document.querySelector('.threadSidebar-1o3BTy').setAttribute('id',this.threadDATA.getThreadSidebarState(BdApi.findModuleByProps('getChannelId').getChannelId())?.channelId);
         }
         if(!document.querySelector('main.chatContent-a9vAAp[guild]')){
             try{let ids = window.location.pathname.match(/\d+/g).reduce((obj, el, index) => { obj[index === 0 ? 'guild' : 'channel'] = el; return obj; }, {})
