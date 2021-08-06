50956083272548354708654967b3dc534dc6f9c0
unknown81311 <43104779+unknown81311@users.noreply.github.com>
2021-08-05 22:44:40 -0600

    Update attrAdder.plugin.js

--- attrAdder.plugin.js
+++ attrAdder.plugin.js
@@ -2,22 +2,26 @@
  * @name attrAdder
  * @author unknown81311#6969
  * @description Adds attributes to elements, can be used for more features in themes.
- * @version 1.4
+ * @version 2
  * @invite https://discord.gg/yYJA3qQE5F
  * @authorId 359174224809689089
  * @source https://github.com/unknown81311/attrAdder
  * @updateUrl https://raw.githubusercontent.com/unknown81311/attrAdder/main/attrAdder.plugin.js
  */
 
+
 module.exports = class attrAdder {
     load() {
         this.threadDATA = BdApi.findModuleByProps('getThreadSidebarState');
         this.channelDATA = BdApi.findModuleByProps('getChannelId');
+        this.getDMFromUserId = BdApi.findModuleByProps("getDMFromUserId", "getChannel");
+        this.getLastSelectedChannelId = BdApi.findModuleByProps("getLastSelectedChannelId", "getChannelId");
     }
     start() {
         if(!document.body.hasAttribute('plugins')){
             const path = require("path");
             const bdpp = path.resolve(path.dirname(BdApi.Plugins.folder), "data", DiscordNative.app.getReleaseChannel(), "plugins.json");
+
             this.watcher = require('fs').watch(bdpp, (eventType, filename) => {
                 document.body.setAttribute('plugins',BdApi.Plugins.getAll().map(p => p.id).filter(BdApi.Plugins.isEnabled));
             });
@@ -32,8 +36,13 @@ module.exports = class attrAdder {
             document.querySelector('.threadSidebar-1o3BTy').setAttribute('id',this.threadDATA.getThreadSidebarState(this.channelDATA.getChannelId())?.channelId);
         }
         if(!document.querySelector('main.chatContent-a9vAAp[guild]')){
-            try{let ids = window.location.pathname.match(/\d+/g).reduce((obj, el, index) => { obj[index === 0 ? 'guild' : 'channel'] = el; return obj; }, {})
-            for (let key in ids){document.querySelector('main').setAttribute(key, ids[key])}}catch{}
+            if(location.href.indexOf('@me') > 0){
+                let userId = this.getDMFromUserId.getChannel(getLastSelectedChannelId.getChannelId()).recipients[0];
+                document.querySelector('main').setAttribute('guild', userId)
+            }else{
+                try{let ids = window.location.pathname.match(/\d+/g).reduce((obj, el, index) => { obj[index === 0 ? 'guild' : 'channel'] = el; return obj; }, {})
+                for (let key in ids){document.querySelector('main').setAttribute(key, ids[key])}}catch{}
+            }
         }
     }
     
