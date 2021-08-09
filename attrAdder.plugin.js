e1c2293ed4bbeb9b58335eb016546d1b870c93c1
unknown81311 <43104779+unknown81311@users.noreply.github.com>
2021-08-09 15:26:57 -0600

    Update attrAdder.plugin.js

--- attrAdder.plugin.js
+++ attrAdder.plugin.js
@@ -2,7 +2,7 @@
  * @name attrAdder
  * @author unknown81311#6969
  * @description Adds attributes to elements, can be used for more features in themes.
- * @version 2.0.0
+ * @version 2.1.0
  * @invite yYJA3qQE5F
  * @authorId 359174224809689089
  * @source https://github.com/unknown81311/attrAdder
@@ -18,18 +18,16 @@ module.exports = class attrAdder {
         this.getLastSelectedChannelId = BdApi.findModuleByProps("getLastSelectedChannelId", "getChannelId");
     }
     start() {
-        if(!document.body.hasAttribute('plugins')){
-            const path = require("path");
-            const bdpp = path.resolve(path.dirname(BdApi.Plugins.folder), "data", DiscordNative.app.getReleaseChannel(), "plugins.json");
+        const path = require("path");
+        const bdpp = path.resolve(path.dirname(BdApi.Plugins.folder), "data", DiscordNative.app.getReleaseChannel(), "plugins.json");
 
-            this.watcher = require('fs').watch(bdpp, (eventType, filename) => {
-                document.body.setAttribute('plugins',BdApi.Plugins.getAll().map(p => p.id).filter(BdApi.Plugins.isEnabled));
-            });
-        }
+        this.watcher = require('fs').watch(bdpp, (eventType, filename) => {
+            document.body.setAttribute('plugins',BdApi.Plugins.getAll().map(p => p.id).filter(BdApi.Plugins.isEnabled));
+        });
     } 
     stop() {
         this.watcher.close();
-        if(document.body.hasAttribute('plugins')) document.body.removeAttribute('plugins');
+        document.body.removeAttribute('plugins');
     }
     observer (mutations) {
         if(document.querySelector('.threadSidebar-1o3BTy') && !document.querySelector('.threadSidebar-1o3BTy[id]')){
